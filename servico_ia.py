1d9491e52ccdc37d227da792427e93abe1e95ecf
leo <leonardo.tamanhao@gmail.com>
2020-07-10 19:23:45 -0300

    fix: ia

--- servico_ia.py
+++ servico_ia.py
@@ -32,10 +32,14 @@ def ia():
     
     imgs_ids=[]
     for item in body[1:]:
-        test = rasterio.open("cropped"+ item["_id"] + ".tif").read(1)
+        raster = rasterio.open("cropped"+ item["_id"] + ".tif").read(1)
+        w = raster.width
+        h = raster.height
         plt.imshow(test)
         plt.show()
-        spec = plt.imshow(test)
+        predicted = predict(raster)
+        sh_preds = np.reshape(predicted, (h,w))
+        spec = plt.imshow(sh_preds)
         plt.savefig(item['_id'] + '.png', bbox_inches='tight', pad_inches=0)
         img_id = upload(item['_id'] + '.png')
         imgs_ids.append(img_id)
@@ -55,14 +59,11 @@ def upload(file):
 
 def predict(x):
     model = load_model('modelo.h5')
-    print(model.summary())
+    
     preds = model.predict(x)
 
-    img_out = []
-    for i in range(0,300):
-        img_out.append([])
-        for j in range(0,300):
-            img_out[i].append(preds[0][1]*300)
+    sh_preds = np.reshape(preds, (h,w))
+
     plt.imshow(img_out)
     plt.show()
 
@@ -71,10 +72,6 @@ def predict(x):
     img_id = upload('image.png')
 
     return img_id
-
-def eps(n):
-  return n > 0.018
-  # return n > 0.045
 
 if __name__ == "__main__":
     app.run(port=8922, debug=True)
